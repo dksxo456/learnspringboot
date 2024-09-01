06d8bd1ff0258fc1dc3b2b404a50768bba32ca1c
Young <Young@192.168.10.125>
2024-09-01 22:02:43 +0900

    first learnspringframework

---
+++ src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
@@ -0,0 +1,44 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
+@Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
+public class GamingAppLauncherApplication {
+	
+ 
+ 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try(var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)){
+			context.getBean(GamingConsole.class).up();
+			
+			context.getBean(GameRunner.class).run();
+			
+		}
+		
+		
+		
+//		var game = new MarioGame();
+//		var game = new SuperContraGame();
+//		var game = new PacmanGame();
+//		
+//		var gameRunner = new GameRunner(game);
+//		gameRunner.run();
+	}
+
+}
+
+
+ 
